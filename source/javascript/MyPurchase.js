1555a94e0d74250eac59cabbd425d1507ff8de7e
Nguyễn Thành <nguyenthanh@Nguyens-MacBook-Pro-2.local>
2023-11-12 17:18:33 +0700

    feedback feedback

---
+++ source/javascript/MyPurchase.js
@@ -0,0 +1,9 @@
+function clickRate(){
+    let x = document.getElementsByClassName('rateBox')
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } 
+    else {
+        x.style.display = 'none';
+    }
+}
