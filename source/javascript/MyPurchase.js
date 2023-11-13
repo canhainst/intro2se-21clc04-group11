be75741aacffefd41ea6113629127326c2f14c32
Nguyễn Thành <nguyenthanh@Nguyens-MacBook-Pro-2.local>
2023-11-13 12:29:31 +0700

    MyPurchase is totally done

--- source/javascript/MyPurchase.js
+++
@@ -1,9 +0,0 @@
-function clickRate(){
-    let x = document.getElementsByClassName('rateBox')
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } 
-    else {
-        x.style.display = 'none';
-    }
-}
