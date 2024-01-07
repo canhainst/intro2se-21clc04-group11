58e32dbe261e4a00f519e7b414a39778ac8ace57
lephuongchi03kh@gmail.com <lephuongchi03kh@gmail.com>
2024-01-07 15:53:10 +0700

    add product to warehouse

--- source/server.js
+++ source/server.js
@@ -34,6 +34,7 @@ app.use('/products', require('./routes_controller/products-r'));
 app.use('/Cart', require('./routes_controller/cart-r'));
 app.use('/book', require('./routes_controller/book-r'));
 app.use('/mypurchase', require('./routes_controller/mypurchase-r.js'));
+app.use('/admin/warehouse', require('./routes_controller/admin-warehouse-r'));
 
 app.get('favorite.ico', (req, res) => {
     res.status(404).send();
